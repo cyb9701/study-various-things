12d3f835c2e50bd323734af3d53d4e582619f56f
cyb9701 <cyb9701@gmail.com>
2024-01-09 16:15:08 +0900

    gsap 라이브러리를 통해 js 애니메이션 효과 추가

--- [Fast campus] 한 번에 끝내는 프론트엔트 개발 초격차 패키지 Online/00.example/02-starbucks-vanilla/js/main.js
+++ [Fast campus] 한 번에 끝내는 프론트엔트 개발 초격차 패키지 Online/00.example/02-starbucks-vanilla/js/main.js
@@ -17,6 +17,22 @@ searchInputEl.addEventListener('blur', () => {
 
 const badgeEl = document.querySelector('header .badges');
 
-window.addEventListener('scroll', () => {
-  console.log('scroll');
-});
+// _.throttle(함수, 시간ms);
+window.addEventListener(
+  'scroll',
+  _.throttle(() => {
+    if (window.scrollY > 500) {
+      // 배지 숨기기.
+      // gsap.to(요소, 지속시간, 옵션);
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: 'none',
+      });
+    } else {
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: 'block',
+      });
+    }
+  }, 300)
+);
