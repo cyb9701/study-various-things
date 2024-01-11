a0123ab9d367d8e642ee5007834d01689d42b966
cyb9701 <cyb9701@gmail.com>
2024-01-11 16:29:28 +0900

    상단으로 이동하는 버튼 추가

--- [Fast campus] 한 번에 끝내는 프론트엔트 개발 초격차 패키지 Online/00. Example/02. starbucks-vanilla/js/main.js
+++ [Fast campus] 한 번에 끝내는 프론트엔트 개발 초격차 패키지 Online/00. Example/02. starbucks-vanilla/js/main.js
@@ -17,6 +17,8 @@ searchInputEl.addEventListener('blur', () => {
 
 const badgeEl = document.querySelector('header .badges');
 
+const toTopEl = document.querySelector('#to-top');
+
 // _.throttle(함수, 시간ms);
 window.addEventListener(
   'scroll',
@@ -28,14 +30,26 @@ window.addEventListener(
         opacity: 0,
         display: 'none',
       });
+      gsap.to(toTopEl, 0.2, {
+        x: 0,
+      });
     } else {
       gsap.to(badgeEl, 0.6, {
         opacity: 1,
         display: 'block',
       });
+      gsap.to('#to-top', 0.2, {
+        x: 100,
+      });
     }
   }, 300)
 );
+
+toTopEl.addEventListener('click', () => {
+  gsap.to(window, 0.7, {
+    scrollTo: 0,
+  });
+});
 
 const fadeEls = document.querySelectorAll('.visual .fade-in');
 
