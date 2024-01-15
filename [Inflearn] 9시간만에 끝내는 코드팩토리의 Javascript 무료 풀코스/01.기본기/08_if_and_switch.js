1c3cbda0e7b0082606b5b9c414f929aaf0787af2
cyb9701 <cyb9701@gmail.com>
2024-01-15 21:22:09 +0900

    if문과 switch문

--- [Inflearn] 9시간만에 끝내는 코드팩토리의 Javascript 무료 풀코스/01.기본기/08_if_and_switch.js
+++ [Inflearn] 9시간만에 끝내는 코드팩토리의 Javascript 무료 풀코스/01.기본기/08_if_and_switch.js
@@ -18,3 +18,31 @@ if (number % 2 === 0) {
 } else {
   console.log('2,3,4의 배수가 아닙니다.');
 }
+
+const englishDay = 'monday';
+let koreanDay;
+
+switch (englishDay) {
+  case 'monday':
+    koreanDay = '월요일';
+    break;
+  case 'tuesday':
+    koreanDay = '화요일';
+    break;
+  case 'wensday':
+    koreanDay = '수요일';
+    break;
+  case 'thursday':
+    koreanDay = '목요일';
+    break;
+  case 'fridday':
+    koreanDay = '금요일';
+    break;
+  case 'saturday':
+    koreanDay = '토요일';
+    break;
+  case 'sunday':
+    koreanDay = '일요일';
+    break;
+}
+console.log(koreanDay);
