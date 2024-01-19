373e18565935bd732941193797ac0030624a2b46
cyb9701 <cyb9701@gmail.com>
2024-01-19 16:56:33 +0900

    class keyword

--- [Inflearn] 9시간만에 끝내는 코드팩토리의 Javascript 무료 풀코스/02.Class and OOP/01_class_keyword.js
+++ [Inflearn] 9시간만에 끝내는 코드팩토리의 Javascript 무료 풀코스/02.Class and OOP/01_class_keyword.js
@@ -8,15 +8,24 @@ class IdolModel {
 const yuJin = new IdolModel();
 console.log(yuJin.name);
 
-// constructor: 생성자
 class IdolModel1 {
   // name;
   // year;
 
+  // constructor: 생성자
   constructor(name, year) {
     this.name = name;
     this.year = year;
   }
+
+  // function
+  sayName() {
+    return `안녕하세요 ${this.name}입니다`;
+  }
 }
 const yuJin1 = new IdolModel1('안유진', 2003);
 console.log(yuJin1); // IdolModel1 { name: '안유진', year: 2003 }
+console.log(yuJin1.sayName()); // 안녕하세요 안유진입니다.
+
+console.log(typeof IdolModel); // function
+console.log(typeof yuJin1); // object
