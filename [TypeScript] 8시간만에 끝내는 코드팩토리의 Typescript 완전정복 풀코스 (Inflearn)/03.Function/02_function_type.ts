f6efb7bb2fdf5a1d0ca5fe39cca7b2f2a6c3c433
cyb9701 <cyb9701@gmail.com>
2024-01-23 21:18:47 +0900

    설명 추가

--- [TypeScript] 8시간만에 끝내는 코드팩토리의 Typescript 완전정복 풀코스 (Inflearn)/03.Function/02_function_type.ts
+++ [TypeScript] 8시간만에 끝내는 코드팩토리의 Typescript 완전정복 풀코스 (Inflearn)/03.Function/02_function_type.ts
@@ -32,6 +32,7 @@ const multiplyTwoNumbers2: InterfaceMultiplyTwoNumbers = (x, y) => {
 /**
  * Function in Interface
  */
+// 함수 타입 선업.
 interface A {
   (input: number): number;
 }
@@ -44,10 +45,22 @@ interface B {
   (input2: number): number;
 }
 
+// 함수가 속성으로 정의.
+// 이 방식은 함수 표현식(function expression)을 사용해서 정의된 것으로, 익명 함수를 변수에 할당하는 방식.
 interface C {
   c: (input: number) => number;
 }
+const cVariable: C = {
+  c: (input) => input * 2,
+};
+console.log(cVariable.c(10));
 
+// 함수는 메서드 시그니처(method signature) 형태로 정의.
+// 이 방식은 함수 선언식(function declaration)을 사용해 정의된 것으로, 함수 이름을 직접 명시하는 방식.
 interface D {
   d(input: number): number;
 }
+const dVariable: D = {
+  d: (input) => input * 2,
+};
+console.log(dVariable.d(10));
