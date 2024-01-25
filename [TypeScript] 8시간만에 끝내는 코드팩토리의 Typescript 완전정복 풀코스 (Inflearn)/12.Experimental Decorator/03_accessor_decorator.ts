705c670b305ed9acc2326d37b87e8c7ee7dc20a9
cyb9701 <cyb9701@gmail.com>
2024-01-25 11:06:19 +0900

    accessor decorator

---
+++ [TypeScript] 8시간만에 끝내는 코드팩토리의 Typescript 완전정복 풀코스 (Inflearn)/12.Experimental Decorator/03_accessor_decorator.ts
@@ -0,0 +1,34 @@
+/**
+ * Accessor Decorator
+ */
+class Rectangle {
+  #height: number;
+  #width: number;
+
+  constructor(height: number, width: number) {
+    this.#height = height;
+    this.#width = width;
+  }
+
+  @Configurable(false)
+  get height() {
+    return this.#height;
+  }
+
+  @Configurable(false)
+  get width() {
+    return this.#width;
+  }
+}
+
+function Configurable(configurable: boolean) {
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {
+    descriptor.configurable = configurable;
+  };
+}
+
+const rectangle = new Rectangle(100, 200);
