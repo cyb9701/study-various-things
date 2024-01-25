38063e4065278e7e0add7988a87a94b46d01d937
cyb9701 <cyb9701@gmail.com>
2024-01-25 09:58:36 +0900

    constructor parameters type

---
+++ [TypeScript] 8시간만에 끝내는 코드팩토리의 Typescript 완전정복 풀코스 (Inflearn)/11.Utility Types/10_constructor_parameters_type.ts
@@ -0,0 +1,14 @@
+/**
+ * Constructor Parameters Type
+ */
+class Idol {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+type ConstructorParamType = ConstructorParameters<typeof Idol>; // TYPE: [name: string, age: number]
