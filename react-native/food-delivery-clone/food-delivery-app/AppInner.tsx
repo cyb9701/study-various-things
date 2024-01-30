e6c8d62170ea65c8b21daedab5a1989c77e7d11f
cyb9701 <cyb9701@gmail.com>
2024-01-30 12:50:45 +0900

    실시간 데이터 받기

--- react-native/food-delivery-clone/food-delivery-app/AppInner.tsx
+++ react-native/food-delivery-clone/food-delivery-app/AppInner.tsx
@@ -1,9 +1,10 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useSelector} from 'react-redux';
 import {RootStackParamList} from './App';
+import useSocket from './src/hooks/useSocket';
 import Delivery from './src/pages/Delivery';
 import Orders from './src/pages/Orders';
 import Settings from './src/pages/Settings';
@@ -16,6 +17,35 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function AppInner() {
   const isLoggedIn = useSelector((state: RootState) => !!state.user.email);
+
+  const [socket, disconnect] = useSocket();
+
+  useEffect(() => {
+    const helloCallback = (data: any) => {
+      console.log(data);
+    };
+    if (socket && isLoggedIn) {
+      console.log(socket);
+
+      // 데이터를 보낸다.
+      socket.emit('login', 'hello');
+
+      // 데이터를 받는다.
+      socket.on('hello', helloCallback);
+    }
+    return () => {
+      if (socket) {
+        socket.off('hello', helloCallback);
+      }
+    };
+  }, [isLoggedIn, socket]);
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      console.log('!isLoggedIn', !isLoggedIn);
+      disconnect();
+    }
+  }, [isLoggedIn, disconnect]);
 
   return (
     <NavigationContainer>
