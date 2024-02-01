7961f14f95320a43c3f7fe92e16f9ddce4018a1b
cyb9701 <cyb9701@gmail.com>
2024-02-01 09:47:41 +0900

    바텀 네비게이션바 추가

--- flutter/practical-skills/app/lib/common/layout/default_layout.dart
+++ flutter/practical-skills/app/lib/common/layout/default_layout.dart
@@ -2,11 +2,15 @@ import 'package:flutter/material.dart';
 
 class DefaultLayout extends StatelessWidget {
   final Widget child;
+  final Widget? bottomNavigationBar;
+  final String? title;
   final Color backgroundColor;
 
   const DefaultLayout({
     super.key,
     required this.child,
+    this.bottomNavigationBar,
+    this.title,
     this.backgroundColor = Colors.white,
   });
 
@@ -14,7 +18,28 @@ class DefaultLayout extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor,
+      appBar: _renderAppBar(),
       body: child,
+      bottomNavigationBar: bottomNavigationBar,
     );
+  }
+
+  AppBar? _renderAppBar() {
+    if (title == null) {
+      return null;
+    } else {
+      return AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        foregroundColor: Colors.black,
+        title: Text(
+          title!,
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      );
+    }
   }
 }
--- flutter/practical-skills/app/lib/common/view/root_tab.dart
+++ flutter/practical-skills/app/lib/common/view/root_tab.dart
@@ -1,13 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:practical_skills/common/const/colors.dart';
 
 import '../layout/default_layout.dart';
 
-class RootTab extends StatelessWidget {
+class RootTab extends StatefulWidget {
   const RootTab({super.key});
 
   @override
+  State<RootTab> createState() => _RootTabState();
+}
+
+class _RootTabState extends State<RootTab> {
+  int _index = 0;
+  @override
   Widget build(BuildContext context) {
     return DefaultLayout(
+      title: '배달의 민족',
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: primaryColor,
+        unselectedItemColor: bodyTextColor,
+        selectedFontSize: 10,
+        unselectedFontSize: 10,
+        type: BottomNavigationBarType.fixed,
+        onTap: (int index) {
+          setState(() {
+            _index = index;
+          });
+        },
+        currentIndex: _index,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home_outlined,
+            ),
+            label: '홈',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.fastfood_outlined,
+            ),
+            label: '음식',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.receipt_long_outlined,
+            ),
+            label: '주문',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person_outline,
+            ),
+            label: '프로필',
+          ),
+        ],
+      ),
       child: Container(),
     );
   }
