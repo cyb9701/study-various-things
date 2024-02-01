922ff5078899b35acf28b0196e2381edba3270a2
cyb9701 <cyb9701@gmail.com>
2024-02-01 18:07:48 +0900

    json serializable 적용

--- flutter/practical-skills/app/lib/restaurant/model/restaurant_model.dart
+++ flutter/practical-skills/app/lib/restaurant/model/restaurant_model.dart
@@ -1,10 +1,18 @@
-import 'package:practical_skills/common/const/data.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+import '../../common/const/data.dart';
+
+part 'restaurant_model.g.dart';
 
 enum RestaurantPriceRange { expensive, medium, cheap }
 
+@JsonSerializable()
 class RestaurantModel {
   final String id;
   final String name;
+  @JsonKey(
+    fromJson: pathToUrl,
+  )
   final String thumbUrl;
   final List<String> tags;
   final RestaurantPriceRange priceRange;
@@ -25,17 +33,24 @@ class RestaurantModel {
     required this.deliveryFee,
   });
 
-  factory RestaurantModel.fromJson(Map<String, dynamic> json) {
-    return RestaurantModel(
-      id: json['id'],
-      name: json['name'],
-      thumbUrl: '$ip${json['thumbUrl']}',
-      tags: List<String>.from(json['tags']),
-      priceRange: RestaurantPriceRange.values.firstWhere((e) => e.name == json['priceRange']),
-      ratings: json['ratings'],
-      ratingsCount: json['ratingsCount'],
-      deliveryTime: json['deliveryTime'],
-      deliveryFee: json['deliveryFee'],
-    );
+  factory RestaurantModel.fromJson(Map<String, dynamic> json) => _$RestaurantModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$RestaurantModelToJson(this);
+
+  static pathToUrl(String value) {
+    return '$ip$value';
   }
+  // factory; RestaurantModel.fromJson(Map<String, dynamic> json) {
+  //   return RestaurantModel(
+  //     id: json['id'],
+  //     name: json['name'],
+  //     thumbUrl: '$ip${json['thumbUrl']}',
+  //     tags: List<String>.from(json['tags']),
+  //     priceRange: RestaurantPriceRange.values.firstWhere((e) => e.name == json['priceRange']),
+  //     ratings: json['ratings'],
+  //     ratingsCount: json['ratingsCount'],
+  //     deliveryTime: json['deliveryTime'],
+  //     deliveryFee: json['deliveryFee'],
+  //   );
+  // }
 }
