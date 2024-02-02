0ae8f65f32d2587aba06ed1aeaf5a60eb022cfd7
cyb9701 <cyb9701@gmail.com>
2024-02-02 13:32:10 +0900

    기본 구조 생성

---
+++ flutter/practical-skills/riverpod-playround/lib/screen/home_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:riverpod_playround/layout/default_layout.dart';
+import 'package:riverpod_playround/screen/state_provider_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+      title: 'Home Screen',
+      body: ListView(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const StateProviderScreen(),
+                ),
+              );
+            },
+            child: const Text(
+              'StateProviderScreen',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
