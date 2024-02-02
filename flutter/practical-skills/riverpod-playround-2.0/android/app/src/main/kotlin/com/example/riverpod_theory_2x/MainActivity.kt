568510124b857939774750ea311253b21ff3c87b
cyb9701 <cyb9701@gmail.com>
2024-02-02 17:02:47 +0900

    프로젝트 제거

--- flutter/practical-skills/riverpod-playround-2.0/android/app/src/main/kotlin/com/example/riverpod_theory_2x/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.riverpod_theory_2x
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- flutter/practical-skills/riverpod-playround-2.0/lib/riverpod/auto_dispose_provider.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:riverpod_theory/repository/multiples_repository.dart';
-
-final autoDisposeProvider = FutureProvider.autoDispose((ref) {
-  return MultiplesRepository.getMultiplesFuture(number: 2);
-});
