e31808f8ea5a9e1c39a26836fe275ffc0aaf5395
cyb9701 <cyb9701@gmail.com>
2024-02-05 10:29:03 +0900

    새로고침 로직

--- flutter/practical-skills/app/lib/restaurant/provider/restaurant_provider.dart
+++ flutter/practical-skills/app/lib/restaurant/provider/restaurant_provider.dart
@@ -30,26 +30,27 @@ class RestaurantStateNotifier extends StateNotifier<CursorPaginationBase> {
     // 강제로 다시 로딩.
     bool forceRefetch = false,
   }) async {
-    /**
+    try {
+      /**
      * 바로 반환하는 상황.
      */
-    // 1) hasMore = false (기존 상태에서 이미 다음 데이터가 없다는 값을 들고 있다면)
-    if (state is CursorPaginationModel && !forceRefetch) {
-      final paginationState = state as CursorPaginationModel;
-      if (!paginationState.meta.hasMore) {
+      // 1) hasMore = false (기존 상태에서 이미 다음 데이터가 없다는 값을 들고 있다면)
+      if (state is CursorPaginationModel && !forceRefetch) {
+        final paginationState = state as CursorPaginationModel;
+        if (!paginationState.meta.hasMore) {
+          return;
+        }
+      }
+
+      // 2) 로딩 중 - fetchMore = true (false - 새로고침의 의도가 있기 때문에 요청을 진행한다.)
+      final isLoading = state is CursorPaginationLoadingState;
+      final isRefetching = state is CursorPaginationRefetchingState;
+      final isFetchingMore = state is CursorPaginationFetchingMoreState;
+      if (fetchMore && (isLoading || isRefetching || isFetchingMore)) {
         return;
       }
-    }
 
-    // 2) 로딩 중 - fetchMore = true (false - 새로고침의 의도가 있기 때문에 요청을 진행한다.)
-    final isLoading = state is CursorPaginationLoadingState;
-    final isRefetching = state is CursorPaginationRefetchingState;
-    final isFetchingMore = state is CursorPaginationFetchingMoreState;
-    if (fetchMore && (isLoading || isRefetching || isFetchingMore)) {
-      return;
-    }
-
-    /**
+      /**
      * 요청.
      * 
      * state
@@ -59,24 +60,40 @@ class RestaurantStateNotifier extends StateNotifier<CursorPaginationBase> {
      * 4) CursorPaginationRefetching - 첫번째 페이지부터 다시 데이터를 가져올 때.
      * 5) CursorPaginationFetchMore - 추가 데이터를 paginate 해오라는 요청을 받았을 때.
      */
-    PaginationParams params = PaginationParams(
-      count: fetchCount,
-    );
-
-    // fetchMore
-    // 데이터를 추갈 더 가져오는 상황
-    if (fetchMore) {
-      final paginationState = state as CursorPaginationModel;
-
-      // ui 업데이트.
-      state = CursorPaginationFetchingMoreState(
-        meta: paginationState.meta,
-        data: paginationState.data,
+      PaginationParams params = PaginationParams(
+        count: fetchCount,
       );
 
-      params = params.copyWith(
-        after: paginationState.data.last.id,
-      );
+      // fetchMore
+      // 데이터를 추갈 더 가져오는 상황
+      if (fetchMore) {
+        final paginationState = state as CursorPaginationModel;
+
+        // ui 업데이트.
+        state = CursorPaginationFetchingMoreState(
+          meta: paginationState.meta,
+          data: paginationState.data,
+        );
+
+        params = params.copyWith(
+          after: paginationState.data.last.id,
+        );
+      }
+
+      // 데이터를 처음부터 가져오는 상황.
+      else {
+        // 만약에 데이터가 있는 상황이라면 기존 데이터로 보존한채로 Fetch를 진행.
+        if (state is CursorPaginationModel && !forceRefetch) {
+          final paginationState = state as CursorPaginationModel;
+          state = CursorPaginationRefetchingState(
+            meta: paginationState.meta,
+            data: paginationState.data,
+          );
+        } else {
+          state = CursorPaginationLoadingState();
+        }
+      }
+
       final response = await repository.paginate(paginationParams: params);
 
       if (state is CursorPaginationFetchingMoreState) {
@@ -89,7 +106,11 @@ class RestaurantStateNotifier extends StateNotifier<CursorPaginationBase> {
             ...response.data,
           ],
         );
+      } else {
+        state = response;
       }
+    } catch (e) {
+      state = CursorPaginationErrorState(message: '데이터를 가져오지 못했습니다.');
     }
   }
 }
