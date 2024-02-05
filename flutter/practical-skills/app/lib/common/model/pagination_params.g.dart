6e3fdd5bb47a16f6245d5757624f528e6a719e79
cyb9701 <cyb9701@gmail.com>
2024-02-05 17:52:13 +0900

    rating model

--- flutter/practical-skills/app/lib/common/model/pagination_params.g.dart
+++ flutter/practical-skills/app/lib/common/model/pagination_params.g.dart
@@ -6,12 +6,14 @@ part of 'pagination_params.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-PaginationParams _$PaginationParamsFromJson(Map<String, dynamic> json) => PaginationParams(
+PaginationParams _$PaginationParamsFromJson(Map<String, dynamic> json) =>
+    PaginationParams(
       after: json['after'] as String?,
       count: json['count'] as int?,
     );
 
-Map<String, dynamic> _$PaginationParamsToJson(PaginationParams instance) => <String, dynamic>{
+Map<String, dynamic> _$PaginationParamsToJson(PaginationParams instance) =>
+    <String, dynamic>{
       'after': instance.after,
       'count': instance.count,
     };
---
+++ flutter/practical-skills/app/lib/rating/model/rating_model.dart
@@ -0,0 +1,25 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:practical_skills/common/utils/data_utils.dart';
+import 'package:practical_skills/user/model/user_model.dart';
+
+part 'rating_model.g.dart';
+
+@JsonSerializable()
+class RatingModel {
+  final String id;
+  final UserModel user;
+  final String rating;
+  final String content;
+  @JsonKey(
+    fromJson: DataUtils.listPathsToUrls,
+  )
+  final List<String> imgUrls;
+
+  RatingModel({
+    required this.id,
+    required this.user,
+    required this.rating,
+    required this.content,
+    required this.imgUrls,
+  });
+}
