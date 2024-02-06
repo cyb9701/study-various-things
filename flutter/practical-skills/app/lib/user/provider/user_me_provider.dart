e6e1a7f74873c893f09b357b7dd5f7b7a6d32749
cyb9701 <cyb9701@gmail.com>
2024-02-06 21:53:05 +0900

    user me provider 적용

--- flutter/practical-skills/app/lib/user/provider/user_me_provider.dart
+++ flutter/practical-skills/app/lib/user/provider/user_me_provider.dart
@@ -2,14 +2,17 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:practical_skills/common/const/data.dart';
 import 'package:practical_skills/user/model/user_model.dart';
+import 'package:practical_skills/user/repository/auth_repository.dart';
 import 'package:practical_skills/user/repository/user_me_repository.dart';
 
 class UserMeStateNotifier extends StateNotifier<UserModelBase?> {
-  final UserMeRepository repository;
+  final AuthRepository authRepository;
+  final UserMeRepository userMeRepository;
   final FlutterSecureStorage storage;
 
   UserMeStateNotifier({
-    required this.repository,
+    required this.authRepository,
+    required this.userMeRepository,
     required this.storage,
   }) : super(UserModelLoading()) {
     getMe();
@@ -24,8 +27,45 @@ class UserMeStateNotifier extends StateNotifier<UserModelBase?> {
       return;
     }
 
-    final response = await repository.getMe();
+    final response = await userMeRepository.getMe();
 
     state = response;
+  }
+
+  Future<UserModelBase> login({
+    required String username,
+    required String password,
+  }) async {
+    try {
+      state = UserModelLoading();
+
+      final response = await authRepository.login(
+        username: username,
+        password: password,
+      );
+
+      await storage.write(key: refreshTokenKey, value: response.refreshToken);
+      await storage.write(key: accessTokenKey, value: response.accessToken);
+
+      final userResponse = await userMeRepository.getMe();
+
+      state = userResponse;
+
+      return userResponse;
+    } catch (_) {
+      state = UserModelError(message: '로그인 실패했습니다.');
+
+      return Future.value(state);
+    }
+  }
+
+  Future<void> logout() async {
+    state = null;
+
+    // Future 동시에 실행.
+    await Future.wait([
+      storage.delete(key: refreshTokenKey),
+      storage.delete(key: accessTokenKey),
+    ]);
   }
 }
