f484880b5bd6a33e081f5c651c80e62e8b3a36f2
cyb9701 <cyb9701@gmail.com>
2024-02-06 18:03:49 +0900

    오류 수정

--- flutter/practical-skills/app/lib/user/provider/user_me_provider.dart
+++ flutter/practical-skills/app/lib/user/provider/user_me_provider.dart
@@ -20,6 +20,7 @@ class UserMeStateNotifier extends StateNotifier<UserModelBase?> {
     final accessToken = await storage.read(key: accessTokenKey);
 
     if (refreshToken == null || accessToken == null) {
+      state = null;
       return;
     }
 
