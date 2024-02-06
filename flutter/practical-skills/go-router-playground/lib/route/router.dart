d1d5d5c770a4bd06e1689f33c8cd696d33fd4a7a
cyb9701 <cyb9701@gmail.com>
2024-02-06 14:30:55 +0900

    go named 함수

--- flutter/practical-skills/go-router-playground/lib/route/router.dart
+++ flutter/practical-skills/go-router-playground/lib/route/router.dart
@@ -1,5 +1,6 @@
 import 'package:go_router/go_router.dart';
 import 'package:go_router_playground/screens/1_basic_screen.dart';
+import 'package:go_router_playground/screens/2_named_screen.dart';
 import 'package:go_router_playground/screens/root_screen.dart';
 
 // https://blog.codefactory.ai -> "/"" -> path
@@ -16,6 +17,20 @@ final router = GoRouter(
           path: 'basic',
           builder: (context, state) {
             return const BasicScreen();
+          },
+        ),
+        GoRoute(
+          path: 'named',
+
+          /**
+           * path가 너무 길어졌을 때 유용하다.
+           * 
+           * 123/123123/123/123/21/312/312/31/212323/
+           * -> login_page
+           */
+          name: 'named_screen',
+          builder: (context, state) {
+            return const NamedScreen();
           },
         ),
       ],
