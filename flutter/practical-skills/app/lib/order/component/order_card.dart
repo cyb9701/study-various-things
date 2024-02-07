5a0e42ea0b21fde0183873ddb83c5aa92e07dc4f
cyb9701 <cyb9701@gmail.com>
2024-02-07 15:02:05 +0900

    order pagination 작업하기

--- flutter/practical-skills/app/lib/order/component/order_card.dart
+++ flutter/practical-skills/app/lib/order/component/order_card.dart
@@ -28,6 +28,7 @@ class OrderCard extends StatelessWidget {
       orderDate: model.createdAt,
       image: Image.network(
         model.restaurant.thumbUrl,
+        width: 50,
         height: 50,
         fit: BoxFit.cover,
       ),
@@ -40,30 +41,33 @@ class OrderCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
-          '${orderDate.year}.${orderDate.month.toString().padLeft(2, '0')}.${orderDate.day.toString().padLeft(2, '0')} 주문완료',
+        Padding(
+          padding: const EdgeInsets.only(bottom: 8),
+          child: Text(
+            '${orderDate.year}.${orderDate.month.toString().padLeft(2, '0')}.${orderDate.day.toString().padLeft(2, '0')} 주문완료',
+          ),
         ),
         Row(
           children: [
             Padding(
-              padding: const EdgeInsets.only(bottom: 16),
+              padding: const EdgeInsets.only(right: 8),
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(16),
                 child: image,
               ),
             ),
             Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
                   name,
-                  style: const TextStyle(
-                    fontSize: 14,
-                  ),
                 ),
                 Text(
                   '$productsDetail $price원',
                   style: const TextStyle(
+                    fontSize: 12,
                     fontWeight: FontWeight.w300,
                   ),
                 ),
--- flutter/practical-skills/app/lib/order/view/order_serceen.dart
+++ flutter/practical-skills/app/lib/order/view/order_serceen.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:practical_skills/common/component/pagination_list_view.dart';
+import 'package:practical_skills/order/component/order_card.dart';
+import 'package:practical_skills/order/provider/order_provider.dart';
 
 class OrderScreen extends ConsumerWidget {
   const OrderScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Placeholder();
+    return PaginationListView(
+      provider: orderProvider,
+      itemBuilder: <OrderModle>(context, index, model) {
+        return OrderCard.fromModel(model: model);
+      },
+    );
   }
 }
