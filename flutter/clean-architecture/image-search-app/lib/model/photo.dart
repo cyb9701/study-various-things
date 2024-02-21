311a871601528a6b6fc6861b3899a078e5fbd974
cyb9701 <cyb9701@gmail.com>
2024-02-21 18:44:06 +0900

    json serializable 적용

--- flutter/clean-architecture/image-search-app/lib/model/photo.dart
+++ flutter/clean-architecture/image-search-app/lib/model/photo.dart
@@ -1,4 +1,33 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'photo.g.dart';
+
+@JsonSerializable()
 class Photo {
+  final int id;
+  final String pageURL;
+  final String type;
+  final String tags;
+  final String previewURL;
+  final int previewWidth;
+  final int previewHeight;
+  final String webformatURL;
+  final int webformatWidth;
+  final int webformatHeight;
+  final String largeImageURL;
+  final String? fullHDURL;
+  final String? imageURL;
+  final int imageWidth;
+  final int imageHeight;
+  final int imageSize;
+  final int views;
+  final int downloads;
+  final int likes;
+  final int comments;
+  final int userId;
+  final String user;
+  final String userImageURL;
+
   Photo({
     required this.id,
     required this.pageURL,
@@ -25,81 +54,7 @@ class Photo {
     required this.userImageURL,
   });
 
-  late final int id;
-  late final String pageURL;
-  late final String type;
-  late final String tags;
-  late final String previewURL;
-  late final int previewWidth;
-  late final int previewHeight;
-  late final String webformatURL;
-  late final int webformatWidth;
-  late final int webformatHeight;
-  late final String largeImageURL;
-  late final String? fullHDURL;
-  late final String? imageURL;
-  late final int imageWidth;
-  late final int imageHeight;
-  late final int imageSize;
-  late final int views;
-  late final int downloads;
-  late final int likes;
-  late final int comments;
-  late final int userId;
-  late final String user;
-  late final String userImageURL;
+  factory Photo.fromJson(Map<String, dynamic> json) => _$PhotoFromJson(json);
 
-  Photo.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    pageURL = json['pageURL'];
-    type = json['type'];
-    tags = json['tags'];
-    previewURL = json['previewURL'];
-    previewWidth = json['previewWidth'];
-    previewHeight = json['previewHeight'];
-    webformatURL = json['webformatURL'];
-    webformatWidth = json['webformatWidth'];
-    webformatHeight = json['webformatHeight'];
-    largeImageURL = json['largeImageURL'];
-    fullHDURL = json['fullHDURL'];
-    imageURL = json['imageURL'];
-    imageWidth = json['imageWidth'];
-    imageHeight = json['imageHeight'];
-    imageSize = json['imageSize'];
-    views = json['views'];
-    downloads = json['downloads'];
-    likes = json['likes'];
-    comments = json['comments'];
-    userId = json['user_id'];
-    user = json['user'];
-    userImageURL = json['userImageURL'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final data = <String, dynamic>{};
-    data['id'] = id;
-    data['pageURL'] = pageURL;
-    data['type'] = type;
-    data['tags'] = tags;
-    data['previewURL'] = previewURL;
-    data['previewWidth'] = previewWidth;
-    data['previewHeight'] = previewHeight;
-    data['webformatURL'] = webformatURL;
-    data['webformatWidth'] = webformatWidth;
-    data['webformatHeight'] = webformatHeight;
-    data['largeImageURL'] = largeImageURL;
-    data['fullHDURL'] = fullHDURL;
-    data['imageURL'] = imageURL;
-    data['imageWidth'] = imageWidth;
-    data['imageHeight'] = imageHeight;
-    data['imageSize'] = imageSize;
-    data['views'] = views;
-    data['downloads'] = downloads;
-    data['likes'] = likes;
-    data['comments'] = comments;
-    data['user_id'] = userId;
-    data['user'] = user;
-    data['userImageURL'] = userImageURL;
-    return data;
-  }
+  Map<String, dynamic> toJson() => _$PhotoToJson(this);
 }
