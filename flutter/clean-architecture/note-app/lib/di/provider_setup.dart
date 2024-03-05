932a4f56a8e5f94d45a5a8c5a47e5a4d4dfb79e4
cyb9701 <cyb9701@gmail.com>
2024-03-05 12:25:52 +0900

    의존성 주입 기본 설정

---
+++ flutter/clean-architecture/note-app/lib/di/provider_setup.dart
@@ -0,0 +1,13 @@
+import 'package:provider/single_child_widget.dart';
+
+List<SingleChildWidget> globalProviders = [
+  ...independentModels,
+  ...dependentModels,
+  ...viewModels,
+];
+
+List<SingleChildWidget> independentModels = [];
+
+List<SingleChildWidget> dependentModels = [];
+
+List<SingleChildWidget> viewModels = [];
