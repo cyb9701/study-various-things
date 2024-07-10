3d3ea7fba3ab89c45ca872c94d8b4bc7d2958267
cyb9701 <cyb9701@gmail.com>
2024-07-10 15:59:24 +0900

    feature: 로또 게임 구현

--- react/react-web-game/06-lotto/Lotto.jsx
+++ react/react-web-game/06-lotto/Lotto.jsx
@@ -1,7 +1,70 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+
+const getLottoNumbers = () => {
+  const candidate = Array(45).fill(0).map((v, i) => i + 1);
+  const shuffle = [];
+
+  while (candidate.length > 0) {
+    const number = candidate.splice(
+      Math.floor(Math.random() * candidate.length),
+      1,
+    )[0];
+    shuffle.push(number);
+  }
+
+  const bonusNumber = shuffle[shuffle.length - 1];
+  const lottoNumbers = shuffle.slice(0, 6).sort((a, b) => a - b);
+
+  return [...lottoNumbers, bonusNumber];
+}
 
 const Lotto = () => {
-  return <div></div>
+  const [lottoNumbers, setLottoNumbers] = useState(getLottoNumbers);
+  const [lottoBalls, setLottoBalls] = useState([]);
+  const [bonusBall, setBonusBall] = useState(null);
+  const [reload, setReload] = useState(false);
+
+  useEffect(() => {
+    let intervalId;
+
+    if (lottoNumbers.length === 1) {
+      intervalId = setInterval(() => {
+        setBonusBall(lottoNumbers[0]);
+        setLottoNumbers([]);
+      }, 1000);
+    } else {
+      const number = lottoNumbers.splice(0, 1)[0];
+      intervalId = setInterval(() => {
+        setLottoBalls((prevState) => {
+          return [...prevState, number];
+        });
+      }, 1000);
+    }
+
+    return () => clearInterval(intervalId);
+  }, [lottoBalls]);
+
+  const handleClickButton = () => {
+    setLottoNumbers(getLottoNumbers());
+    setLottoBalls([]);
+    setBonusBall(null);
+  }
+
+  return (
+    <>
+      <div>당첨 번호</div>
+      {
+        lottoBalls.length !== 0 && lottoBalls.map((e) => <div key={e}>{e}</div>)
+      }
+      <div>보너스</div>
+      {
+        bonusBall && <div>{bonusBall}</div>
+      }
+      {
+        lottoNumbers.length === 0 && <button onClick={handleClickButton}>reload</button>
+      }
+    </>
+  );
 }
 
 export default Lotto;
