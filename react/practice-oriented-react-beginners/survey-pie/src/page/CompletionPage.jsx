afe3b88c01a4bbf1779ad118a59cfbde29223029
cyb9701 <cyb9701@gmail.com>
2025-02-10 11:04:46 +0900

    add: 서버 프로젝트 추가

--- react/practice-oriented-react-beginners/survey-pie/src/page/CompletionPage.jsx
+++ react/practice-oriented-react-beginners/survey-pie/src/page/CompletionPage.jsx
@@ -1,6 +1,16 @@
+import axios from 'axios';
 import styled from 'styled-components';
 
 const CompletionPage = () => {
+  axios
+    .get('http://localhost:5173/')
+    .then((response) => {
+      console.log('🍀CompletionPage:8🍀', response.data);
+    })
+    .catch((error) => {
+      console.log('🍀CompletionPage:11🍀', error);
+    });
+
   return <CompletionPageWrapper>aaa</CompletionPageWrapper>;
 };
 
